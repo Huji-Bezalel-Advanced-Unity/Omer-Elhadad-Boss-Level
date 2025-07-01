a05aa5f466767aee0505ae9ae474f4917424d376
omerelhadad2000 <omer.elh11@gmail.com>
2025-07-01 19:51:14 +0300

    Adding Enemy Behaviour - Fire Attack

--- BossLevel/Assets/Scripts/Boss/First Stage/FirstStageMovement.cs
+++ BossLevel/Assets/Scripts/Boss/First Stage/FirstStageMovement.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class FirstStageMovement : MonoBehaviour
 {
-    public Transform playerTransform;
     
     public bool IsFlipped { get; private set; }
     
     // Flip the boss to face the player
     
-    public void FlipToPlayer()
+    public void FlipToPlayer(Transform playerTransform)
     {
         var flipped = transform.localScale;
         flipped.z *= -1f;
@@ -24,10 +23,5 @@ public class FirstStageMovement : MonoBehaviour
             transform.Rotate(0f, 180f, 0f);
             IsFlipped = false;
         }
-    }
-
-    public float GetXDistanceToPlayer()
-    {
-        return Mathf.Abs(playerTransform.position.x - transform.position.x);
     }
 }
